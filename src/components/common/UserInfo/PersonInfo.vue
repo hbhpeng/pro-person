be072d33b2677583c77afcafbd41f260140fcc7f
wuzm <wuzm@rd.netease.com>
2023-04-27 16:03:20 +0800

    fix: add userinfo

---
+++ src/components/common/UserInfo/PersonInfo.vue
@@ -0,0 +1,135 @@
+<script setup lang='ts'>
+import { NSpin, useMessage } from 'naive-ui'
+import { computed, ref, watch } from 'vue'
+import { useAuthStoreWithout } from '@/store/modules/auth'
+import {
+  getSingleUserInfo,
+} from '@/api'
+
+const props = defineProps<Props>()
+
+const emit = defineEmits<Emit>()
+
+const authStore = useAuthStoreWithout()
+
+interface Props {
+  visible: boolean
+}
+
+interface Emit {
+  (e: 'update:visible', visible: boolean): void
+}
+
+const showModal = computed({
+  get: () => props.visible,
+  set: (visible: boolean) => emit('update:visible', visible),
+})
+const username = ref('')
+const totalChars = ref(0)
+const usedChars = ref(0.0)
+const ms = useMessage()
+let loading = false
+
+const requestUserInfo = () => {
+  try {
+    loading = true
+    getSingleUserInfo().then((res) => {
+      const userInfo = JSON.parse(res.data as any)
+      username.value = userInfo.username as string
+      totalChars.value = userInfo.usagecount as number
+      usedChars.value = userInfo.usecount as number
+      ms.success(res.message ?? '')
+      loading = false
+    }).catch((reason: any) => {
+      ms.success(reason.message ?? '')
+      loading = false
+    })
+  }
+  catch (error: any) {
+    ms.error(error.message ?? '')
+    loading = false
+  }
+}
+
+watch(
+  () => showModal.value,
+  (value) => {
+    if (value)
+      setTimeout(requestUserInfo, 100)
+  },
+  { deep: true },
+)
+
+const loginOut = () => {
+  showModal.value = false
+  authStore.$state.userToken = ''
+  authStore.removeUserT()
+}
+</script>
+
+<template>
+  <NSpin :show="loading">
+    <!-- 自定义模态框 -->
+    <div v-if="showModal" class="modal">
+      <div class="modal-content">
+        <button class="close-button" @click="showModal = false">
+          X
+        </button>
+        <h2>{{ username }} 的详细信息</h2>
+        <p>总字数：{{ totalChars }}万</p>
+        <p>可用字数：{{ (totalChars - usedChars) * 10000 }}</p>
+        <p>已用字数：{{ usedChars }}万</p>
+        <button type="submit" @click.prevent="loginOut">
+          退出登录
+        </button>
+      </div>
+    </div>
+  </NSpin>
+</template>
+
+<style>
+.modal {
+position: fixed;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+background-color: rgba(0, 0, 0, 0.4);
+display: flex;
+justify-content: center;
+align-items: center;
+z-index: 1000;
+}
+
+.modal-content {
+background-color: white;
+padding: 2rem;
+border-radius: 10px;
+box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.5);
+position: relative;
+}
+
+.close-button {
+position: absolute;
+top: 0;
+right: 0;
+font-size: 1.5rem;
+background: none;
+border: none;
+padding: 0.5rem;
+cursor: pointer;
+}
+
+button {
+margin-top: 1rem;
+}
+
+button[type='submit'] {
+width: 100%;
+margin-top: 20px;
+padding: 5px;
+background-color: #6AA1E7;
+border-radius: 5px;
+margin-bottom: 5px;
+}
+</style>
---
+++ src/components/common/UserInfo/index.vue
@@ -0,0 +1,256 @@
+<script setup lang='ts'>
+import {
+  NSpin,
+  useMessage,
+} from 'naive-ui'
+import {
+  computed,
+  ref,
+  watch,
+} from 'vue'
+import {
+  useAuthStoreWithout,
+} from '@/store/modules/auth'
+import {
+  userActionLogin,
+  userActionRegist,
+} from '@/api'
+
+const props = defineProps < Props > ()
+
+const emit = defineEmits < Emit > ()
+
+const authStore = useAuthStoreWithout()
+
+interface Props {
+  visible: boolean
+}
+
+interface Emit {
+  (e: 'update:visible', visible: boolean): void
+}
+
+const showModal = computed({
+  get: () => props.visible,
+  set: (visible: boolean) => emit('update:visible', visible),
+})
+
+// const showModal = ref(false)
+const loginFormVisible = ref(true)
+const registerFormVisible = ref(false)
+const username = ref('')
+const password = ref('')
+const ms = useMessage()
+let loading = false
+
+watch(
+  () => showModal,
+  (value) => {
+    if (value) {
+      username.value = ''
+      password.value = ''
+    }
+  }, {
+    deep: true,
+  },
+)
+
+// const openModal = () => {
+//   showModal.value = true
+// }
+
+const closeModal = () => {
+  showModal.value = false
+}
+
+const showLoginForm = () => {
+  loginFormVisible.value = true
+  registerFormVisible.value = false
+}
+
+const showRegisterForm = () => {
+  loginFormVisible.value = false
+  registerFormVisible.value = true
+}
+
+const validateUsername = (name: string) => {
+  const regex = /^[a-zA-Z0-9_]{6,20}$/ // 匹配包含字母、数字和下划线的6-20个字符
+  return regex.test(name)
+}
+
+const validatePassword = (mima: string) => {
+  const regex = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]{8,16}$/ // 匹配包含大写字母、小写字母、数字和特殊字符的8-16个字符
+  return regex.test(mima)
+}
+
+const validateCanNext = () => {
+  if (!validateUsername(username.value)) {
+    ms.error('用户名不合法')
+    return false
+  }
+  if (!validatePassword(password.value)) {
+    ms.error('密码不合法')
+    return false
+  }
+  return true
+}
+
+const loginOrRegister = async (isLogin: boolean) => {
+  try {
+    loading = true
+    let userdata: any
+    if (isLogin)
+      userdata = await userActionLogin(username.value, password.value)
+
+    else
+      userdata = await userActionRegist(username.value, password.value)
+
+    const message: string = userdata.message as string
+    const data: string = userdata.data as string
+    const {
+      token,
+    } = JSON.parse(data as any)
+    showModal.value = false
+    authStore.$state.userToken = token
+    authStore.setUserT(token)
+    ms.success(message ?? '')
+  }
+  catch (error: any) {
+    ms.error(error.message ?? '')
+  }
+  finally {
+    loading = false
+  }
+}
+
+const loginAction = async () => {
+  if (validateCanNext())
+    loginOrRegister(true)
+}
+const registerAction = async () => {
+  if (validateCanNext())
+    loginOrRegister(false)
+}
+</script>
+
+<template>
+  <NSpin :show="loading">
+    <div v-if="showModal" class="modal">
+      <div class="modal-content">
+        <button class="close-button" @click="closeModal">
+          X
+        </button>
+
+        <div v-if="loginFormVisible">
+          <!-- <h2>登录</h2> -->
+          <form>
+            <label for="email">账号:</label>
+            <input id="email" v-model="username" type="email" name="email" placeholder="字母、数字和下划线的6-20个字符">
+
+            <label for="password">密码:</label>
+            <input id="password" v-model="password" type="password" name="password" placeholder="8-16个字符">
+
+            <button type="submit" @click.prevent="loginAction">
+              立即登录
+            </button>
+          </form>
+
+          <p>没有账号? <a class="tap-a" href="#" @click.prevent="showRegisterForm">现在注册!</a></p>
+        </div>
+
+        <div v-if="registerFormVisible">
+          <h2>注册</h2>
+          <form>
+            <label for="email">账号:</label>
+            <input id="email" v-model="username" type="email" name="email" placeholder="字母、数字和下划线的6-20个字符">
+
+            <label for="password">密码:</label>
+            <input id="password" v-model="password" type="password" name="password" placeholder="8-16个字符">
+
+            <button type="submit" @click.prevent="registerAction">
+              立即注册并登录
+            </button>
+          </form>
+
+          <p>已经有账户? <a class="tap-a" href="#" @click.prevent="showLoginForm">立即登录!</a></p>
+        </div>
+      </div>
+    </div>
+  </NSpin>
+</template>
+
+<style>
+.modal {
+position: fixed;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+background-color: rgba(0, 0, 0, 0.4);
+display: flex;
+justify-content: center;
+align-items: center;
+z-index: 1000;
+}
+
+.modal-content {
+min-width: 300px;
+background-color: white;
+padding: 2rem;
+border-radius: 10px;
+box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.5);
+position: relative;
+}
+
+.close-button {
+position: absolute;
+top: 0;
+right: 0;
+font-size: 1.5rem;
+background: none;
+border: none;
+padding: 0.5rem;
+cursor: pointer;
+}
+
+form {
+display: flex;
+flex-direction: column;
+margin-top: 1rem;
+}
+
+label {
+font-weight: bold;
+margin-bottom: 0.25rem;
+}
+
+button[type='submit'] {
+padding: 10px;
+background-color: #6AA1E7;
+border-radius: 5px;
+margin-bottom: 5px;
+}
+
+a.tap-a {
+color: #6AA1E7;
+/* 设置链接文本颜色 */
+text-decoration: underline;
+/* 给链接下划线 */
+cursor: pointer;
+/* 鼠标悬浮时更改光标形状 */
+}
+
+input[type="email"],
+input[type="password"],
+input[type="text"] {
+padding: 0.5rem;
+margin-bottom: 1rem;
+/* border: none; */
+border: 0.125rem solid gray;
+border-radius: 5px;
+}
+
+input::placeholder {
+font-size: 12px;
+}
+</style>
